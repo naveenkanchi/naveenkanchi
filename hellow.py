ca3598371b7d409cbcd495b73de2836c74d1c670
Naveen Kanchi <100524915+naveenkanchi@users.noreply.github.com>
2022-06-16 08:24:02 +0530

    Add files via upload

---
+++ hellow.py
@@ -0,0 +1,239 @@
+# a="Python" ; b="Programming"
+# print(a+b)
+# print(a,b)
+# print(a+' '+b)
+# print(a+chr(32)+b)
+
+# a="5"; b="7"; c=8
+# print(a,b+str(c))
+# print(a+" "+b+str(c))
+
+
+
+
+# print(""Python programming"")
+# print("""""python programming""")
+# print(""""""python programming""")
+# print("""""""python programming""")
+# print(""""""""python programming""")
+# print("""python programming"""")
+# print("""python programming""""")
+# print("""python programming""""")
+
+'''sep.py'''
+# print('hello' ,'world')
+# print('hello' +' '+chr(32)+'world')
+# print('hello'+chr(32)+"world")
+# print("pyhton"+' '+'programming'+' '+"world")
+# print('hello',"world",sep="*")
+# print("p",'y','t','h','o','n',sep="@")
+
+# print("hello",end=' '"welcome to josh")
+
+
+# print("hello",end="-"'welocmeto josh')
+
+# print("hello",end=","'welcome to josh')
+# print(len("pyhotn"))
+
+# a="python"
+# print(a,type(a))
+# print(a[0])
+# print("python"[0])
+# print("python"[1])
+# print("python"[2])
+# print("python"[3])
+# print("python"[4])
+# print("python"[5])
+# print("python"[6])
+
+# v="department"
+# print(v[3]+v[2]+v[3]+v[4]+v[5]+v[6])
+
+# print(a[-7])
+
+# from unicodedata import name
+
+
+# w="python"
+# print(w[:1])
+# print(w[0:2])
+# print(w[0:3])
+# print(w[0:4])
+# print(w[:])
+# print(w[:])
+# print(w[0:])
+# print(w[0:6])
+# print(w[-6:6])
+# print(w[0:0])
+# print(w[0:5])
+# print(w[0:6])
+# print(w[0:-7])
+# print(w[0:99])
+
+
+# r="python"
+# print(r[2:5])
+# print(r[3:4])
+# print(r[5:2])
+# print(r[5:5])
+
+# c="python"
+# print(c[ : : ])
+# print(c[0: :1])
+# print(c[ : :2])
+# print(c[ : :3])
+# print(c[ : :4])
+# print(c[ : :5])
+# print(c[ : :6])
+# print(c[ : :7])
+# print(c[ : :-1])
+# print(c[ : :-2])
+# print(c[ : :-3])
+# print(c[ : :-4])
+# print(c[ : :-5])
+# print(c[ : :-6])
+# print(c[1: : ])
+# print(c[2: : ])
+# print(c[3: : ])
+# print(c[4: : ])
+# print(c[5: : ])
+# print(c[6: : ])
+# print(c[1: 6: ])
+# print(c[2: 5: ])
+# print(c[3: 4: ])
+# print(c[4: 3: ])
+# print(c[5: 2: ])
+# print(c[6: 1: ])
+
+# a="python"
+# a[0]='p'
+
+# print(dir(str))
+
+# name="hello python"
+# print(name.capitalize())
+
+# name="hello python"
+# print(name.casefold())
+# print(name.lower())
+
+# name="hello pyhton"
+# print(name.casefold())
+# print(name.center())
+
+# name="hello python"
+# print(name.center)
+# print(name.center())
+# print(name.center(50,'-'))
+# print(name.center(100))
+
+# name="hello python"
+# print(type(name))
+# n=name.encode()
+# print(n)
+# print(n.decode())
+
+
+# name='hello python'
+# print(name.upper())
+# print(name.lower())
+# print(name.islower())
+# print(name.isupper())
+# b='CRICKET'
+# print(b.isupper)
+# m='worldcup t20'
+# print(m.islower())
+
+# t="hello python"
+# print(t.title())
+# print(t.capitalize())
+# print(t.istitle())
+# c="hello python"
+# print(c.istitle())
+
+
+# n="hello pyhton"
+# print(n.endswitch(""))
+# print(n.endswitch("  "))
+
+# n='hello python'
+# print(n.startswith(''))
+# print(n.startswith(' '))
+# print(n.startswith('h'))
+
+# z='hello pyhton'
+# print(z.startswitch(''))
+# print(z.startswitch(' '))
+# print(z.startswitch('h'))
+
+# v=''
+# g='   '
+# print(v.isspace())
+# print(g.isspace())
+
+
+# q='hello pyhton'
+# print(q.index('1'))
+
+
+
+# r="hello python programming"
+# print(len(r))
+# print(len('josh innovations'))
+
+
+# b="python programming"
+# print(b.find('p'))
+# print(b.find('e'))
+# print(len(b))
+# print(b.rfind('m'))
+
+# v="python programming"
+# print(v.count('p'))
+# print(v.count('m'))
+# print(v.count('i'))
+# print(v.count('e'))
+
+# h="pyhton course"
+# print(h.replace('course','programming'))
+# g=" core python programming core"
+# print(g.replace('core',str(2)))
+
+# b='pyton programming'
+# print(b.find('p'))
+# print(b.find('e'))
+# print(len(b))
+# print(b.rfind('m'))
+
+
+# v="python programming"
+# print(v.count('p'))
+# print(v.count('m'))
+# print(v.count('i'))
+# print(v.count('e'))
+
+
+# h="python course"
+# print (h.replace('course','programming'))
+# g="core python programming core"
+# print(g.replace('core',str(2)))
+
+# d="    python programming  . "
+# print(d.strip())
+# print(d.lstrip()),
+
+
+# print('hello')
+# print('hey python'*5)
+# a=['10'],['20']
+# print((a))
+# a=12
+# print(type(a))
+
+# a=('hello python')
+# print(type(a))
+# print(a)
+
+n= 1,2,3,4
+print(n,type(n))
